80f4d520fcf77a289ad2f70ddfd40e8a1ed38db8
samaryph <toinette@gmail.com>
2021-12-28 01:27:31 +0800

    let push

--- EclipseTestProject1/src/GitTestingV1.java
+++ EclipseTestProject1/src/GitTestingV1.java
@@ -3,7 +3,7 @@ public class GitTestingV1 {
 
 	public static void main(String[] args) {
 		int loopVal;
-		int end_value = 20;
+		int end_value = 10;
 
 		for (loopVal = 0; loopVal < end_value; loopVal++) {
 			System.out.println("Loop Value = " + loopVal);
