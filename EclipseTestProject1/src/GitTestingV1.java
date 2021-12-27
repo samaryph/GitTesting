e96738c8443ce103a6b31d22c1820b5cd1ca7cf2
samaryph <toinette@gmail.com>
2021-12-28 01:24:30 +0800

    The first commit

---
+++ EclipseTestProject1/src/GitTestingV1.java
@@ -0,0 +1,14 @@
+
+public class GitTestingV1 {
+
+	public static void main(String[] args) {
+		int loopVal;
+		int end_value = 20;
+
+		for (loopVal = 0; loopVal < end_value; loopVal++) {
+			System.out.println("Loop Value = " + loopVal);
+		}
+
+	}
+
+}
